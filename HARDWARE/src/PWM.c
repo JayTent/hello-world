2201862a173a8334f275d3cf1492d7acb5b25f64
JayTent <41749796+JayTent@users.noreply.github.com>
2019-05-16 00:40:09 +0800

    Add files via upload

---
+++ HARDWARE/src/PWM.c
@@ -0,0 +1,48 @@
+#include "PWM.h"
+
+void PWMInit(u16 arr, u16 psc){
+	GPIO_InitTypeDef        GPIO_InitStructure;
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;  
+  TIM_OCInitTypeDef       TIM_OCInitStructure;
+	
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+	
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6|GPIO_Pin_7|GPIO_Pin_8|GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_TIM4);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_TIM4);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource8, GPIO_AF_TIM4);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource9, GPIO_AF_TIM4);
+	
+	TIM_TimeBaseStructure.TIM_Prescaler = psc;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+  TIM_TimeBaseStructure.TIM_Period = arr;
+  TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+  TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
+	
+  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;  
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;  
+  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+	TIM_OCInitStructure.TIM_Pulse = Channel1Pulse;
+  TIM_OC1Init(TIM4, &TIM_OCInitStructure);
+  TIM_OCInitStructure.TIM_Pulse = Channel2Pulse;
+  TIM_OC2Init(TIM4, &TIM_OCInitStructure);
+  TIM_OCInitStructure.TIM_Pulse = Channel3Pulse;
+  TIM_OC3Init(TIM4, &TIM_OCInitStructure);
+  TIM_OCInitStructure.TIM_Pulse = Channel4Pulse;
+  TIM_OC4Init(TIM4, &TIM_OCInitStructure);
+	
+	TIM_OC1PreloadConfig(TIM9, TIM_OCPreload_Enable);
+	TIM_OC2PreloadConfig(TIM9, TIM_OCPreload_Enable);
+	TIM_OC3PreloadConfig(TIM9, TIM_OCPreload_Enable);
+	TIM_OC4PreloadConfig(TIM9, TIM_OCPreload_Enable);
+	
+	TIM_ARRPreloadConfig(TIM14,ENABLE);
+	TIM_Cmd(TIM4, ENABLE);
+	TIM_CtrlPWMOutputs(TIM4, ENABLE);
+}
