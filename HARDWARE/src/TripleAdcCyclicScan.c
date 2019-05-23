0805c988e39c4d1f0c5493ad6355b9a8f23caae1
JayTent <41749796+JayTent@users.noreply.github.com>
2019-05-23 22:52:04 +0800

    Update and rename adcdma.c to TripleAdcCyclicScan.c

--- HARDWARE/src/TripleAdcCyclicScan.c
+++ HARDWARE/src/TripleAdcCyclicScan.c
@@ -1,9 +1,19 @@
-#include "adc.h"
+/********************************************************************************
+ Author : jaytent (jaytent0704@gmail.com)
 
-extern unsigned char Send_OK;
-volatile unsigned int AD_Value[8][3];
+ Date : May 2019
 
-void  ADCInit(void){
+ File name : TripleAdcCyclicScan.c
+
+ Description :¸该可执行文件定义了STM32F4三重ADC循环扫描的所有配置
+
+ Hardware Platform : STM32F407
+********************************************************************************/
+#include "TripleAdcCyclicScan.h"
+
+volatile unsigned int AD_Value[10][6];//AD采样寄存器
+
+void  AdcInit(void){
   GPIO_InitTypeDef       GPIO_InitStructure;
   DMA_InitTypeDef        DMA_InitStructure;
   ADC_CommonInitTypeDef  ADC_CommonInitStructure;
@@ -22,37 +32,36 @@ void  ADCInit(void){
   
   DMA_InitStructure.DMA_Channel = DMA_Channel_0;
   DMA_InitStructure.DMA_PeripheralBaseAddr = (u32)ADC_CDR_ADDRESS;
-  DMA_InitStructure.DMA_Memory0BaseAddr = (u32)AD_Value;//ÄÚ´æµØÖ·
-  DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralToMemory;//ÍâÉèµ½´æ´¢Æ÷
-  DMA_InitStructure.DMA_BufferSize = 60;//Êý¾ÝÁ¿
-  DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;//ÍâÉè·ÇÔöÁ¿Ä£Ê½
-  DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;//´æ´¢Æ÷ÔöÁ¿Ä£Ê½
-  DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;//ÍâÉèÊý¾Ý³¤¶È°ë×Ö
-  DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;//´æ´¢Æ÷Êý¾Ý³¤¶È°ë×Ö
-  DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;//Ñ­»·Ä£Ê½
+  DMA_InitStructure.DMA_Memory0BaseAddr = (u32)AD_Value;
+  DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralToMemory;
+  DMA_InitStructure.DMA_BufferSize = 60;
+  DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+  DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+  DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+  DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+  DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
   DMA_InitStructure.DMA_Priority = DMA_Priority_VeryHigh;
   DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;
   DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_HalfFull;
-  DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single;//´æ´¢Æ÷Í»·¢µ¥´Î´«Êä
-  DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;//ÍâÉèÍ»·¢µ¥´Î´«Êä
+  DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single;
+  DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
   DMA_Init(DMA2_Stream0, &DMA_InitStructure);
   DMA_Cmd(DMA2_Stream0, ENABLE);
-	
-	/* Configure ADC1 */
-  ADC_CommonInitStructure.ADC_Mode = ADC_TripleMode_RegSimult;//ÈýÖØ¹æÔòÍ¬²½Ä£Ê½
-  ADC_CommonInitStructure.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_5Cycles;//¶àÖØÄ£Ê½ÏÂÁ½¸ö²ÉÑù½×¶ÎÖ®¼äµÄÑÓ³Ù5¸öÊ±ÖÓ
-  ADC_CommonInitStructure.ADC_DMAAccessMode = ADC_DMAAccessMode_1;//DMAÄ£Ê½1
-  ADC_CommonInitStructure.ADC_Prescaler = ADC_Prescaler_Div2;//2·ÖÆµ¡£ADCCLK=PCLK2/4=72/2=36Mhz,ADCÊ±ÖÓ×îºÃ²»Òª³¬¹ý36Mhz
+  
+  ADC_CommonInitStructure.ADC_Mode = ADC_TripleMode_RegSimult;
+  ADC_CommonInitStructure.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_5Cycles;
+  ADC_CommonInitStructure.ADC_DMAAccessMode = ADC_DMAAccessMode_1;
+  ADC_CommonInitStructure.ADC_Prescaler = ADC_Prescaler_Div2;
   ADC_CommonInit(&ADC_CommonInitStructure);
   ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
   ADC_InitStructure.ADC_ScanConvMode = ENABLE;
   ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
-  ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;//½ûÖ¹´¥·¢¼ì²â£¬Ê¹ÓÃÈí¼þ´¥·¢
-  ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;//ÓÒ¶ÔÆë
-  ADC_InitStructure.ADC_NbrOfConversion = 2;//2¸ö×ª»»ÔÚ¹æÔòÐòÁÐÖÐ
-	
+  ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
+  ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+  ADC_InitStructure.ADC_NbrOfConversion = 2;
+  
   ADC_Init(ADC1, &ADC_InitStructure);
-  ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_56Cycles);//²ÉÑùÊ±¼äÌ«¶Ì£¬É¨ÃèÍ¨µÀ¼ä»á·¢ÉúÐÅºÅñîºÏ´Ó¶øÓ°ÏìÏÂÒ»ADÐòÁÐ
+  ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_56Cycles);
   ADC_RegularChannelConfig(ADC1, ADC_Channel_5, 2, ADC_SampleTime_56Cycles);
   ADC_Init(ADC2, &ADC_InitStructure);
   ADC_RegularChannelConfig(ADC2, ADC_Channel_3, 1, ADC_SampleTime_56Cycles);
@@ -68,7 +77,7 @@ void  ADCInit(void){
   ADC_Cmd(ADC3, ENABLE);
   
   NVIC_InitStructure.NVIC_IRQChannel=DMA2_Stream0_IRQn;
-  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x02;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x01;
   NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x01;
   NVIC_InitStructure.NVIC_IRQChannelCmd=ENABLE;
   NVIC_Init(&NVIC_InitStructure);
@@ -77,10 +86,13 @@ void  ADCInit(void){
   DMA_ITConfig(DMA2_Stream0, DMA_IT_TC, ENABLE);
 }
 
+void AdcStart(void){
+  ADC_SoftwareStartConv(RHEOSTAT_ADC1);
+}
 
 void DMA2_Stream0_IRQHandler(void){
   if(DMA_GetFlagStatus(DMA2_Stream0, DMA_IT_TCIF0) == SET){
-    Send_OK=1;
+    ;//事件标志或者信号量或者用户自定义标志位
     DMA_ClearFlag(DMA2_Stream0, DMA_IT_TCIF0);
   }
 }
